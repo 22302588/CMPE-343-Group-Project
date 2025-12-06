6e9ebfe21b8b657d6c9f4936a811dcbdca331f78
Lamine Diamoutene <pridila.2006@gmail.com>
2025-12-06 16:43:19 +0200

    back to default

--- department.sql
+++ department.sql
@@ -1,14 +1,14 @@
-Create Table department( 
-department_id int Primary KEY, 
-name varchar(50) not null,
-building varchar(50),
-phone int, 
-department_code varchar(10) not NULL);
+-- Create Table department( 
+-- department_id int Primary KEY, 
+-- name varchar(50) not null,
+-- building varchar(50),
+-- phone int, 
+-- department_code varchar(10) not NULL);
 
-Insert into department(department_id, name, building, phone, department_code) 
-Values 
-(1, 'Maria', 'ST', '+9053629137', 'CMPE'),
-(2, 'Manal', 'CE', '+9610372916', 'HEDR'),
-(3, 'Rand', 'GE', '+971026289', 'EELE');
+-- Insert into department(department_id, name, building, phone, department_code) 
+-- Values 
+-- (1, 'Maria', 'ST', '+9053629137', 'CMPE'),
+-- (2, 'Manal', 'CE', '+9610372916', 'HEDR'),
+-- (3, 'Rand', 'GE', '+971026289', 'EELE');
 
 SELECT * from department;
