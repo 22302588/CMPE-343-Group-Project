a38ffb8743d891ec0baca305f738f7489b03e21f
22314432 <22314432@student.ciu.edu.tr>
2025-12-07 23:39:16 +0200

    ,

--- noura/select.sql
+++ noura/select.sql
@@ -4,10 +4,41 @@ SELECT * FROM departments;
 
 SELECT * FROM programs;
 
-
+/*Display 4*/
 SELECT universities.name, departments.name
 FROM departments
 JOIN universities ON universities.id = departments.university_id
 ORDER BY universities.name ASC,
  departments.name ASC;
- 
+
+/*Display 5*/
+SELECT program.id 
+FROM programs 
+JOIN universities
+WHERE address LIKE "%USA%"
+
+/*Dispaly 6*/
+SELECT program.name , department.name
+From  programs
+JOIN programs ON program.name = departments.program_id
+GROUP BY programs_department
+
+
+/*Display 7*/ 
+SELECT universities.id 
+FROM universities 
+JOIN universities ON universities.id= departments.university_id 
+where "department"=5
+
+/*Display 8*/
+SELECT programs.id 
+FROM programs
+LEFT JOIN programs ON students.id = programs.student_id 
+WHERE "students"= NULL
+/*Display 9*/
+
+
+/*Display 10*/
+SELECT 
+FROM departrments
+
