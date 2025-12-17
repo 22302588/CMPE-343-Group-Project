db0f4a231d8386b351a19e0e2afe391e0aa3ae37
22321330 <22321330@student.ciu.edu.tr>
2025-12-17 17:38:46 +0300

    clean up

--- liya/insert.sql
+++ liya/insert.sql
@@ -1,28 +1,29 @@
-INSERT INTO transcripts(id, student_id, gpa, cgpa, result_date)
+INSERT INTO transcripts(id,course_id, gpa, cgpa, result_date)
 VALUES
-(101, 1, 3.03, 2.99, '2022-11-30'),
-(102, 2, 2.77, 2.87, '2023-09-12'),
-(103, 3, 3.80, 3.76, '2024-09-21'),
-(104, 4, 2.85, 3.06, '2024-06-18'),
-(105, 5, 2.21, 1.99, '2021-07-04');
+(1, 1, 3.03, 2.99, '2022-11-30'),
+(2, 2,  2.77, 2.87, '2023-09-12'),
+(3, 3, 3.80, 3.76, '2024-09-21'),
+(4, 4, 2.85, 3.06, '2024-06-18'),
+(5, 5, 2.21, 1.99, '2021-07-04');
+
 INSERT INTO course_results(id, course_id, transcript_id, grade)
 VALUES
-(001, 10, 101, 40.00 ),
-(002, 22, 102, 67.00),
-(003, 33, 103, 38.99),
-(004, 44, 104, 95.0), 
-(005, 55, 105, 87.5);
+(001, 1, 101, 40.00 ),
+(002, 2, 102, 67.00),
+(003, 3, 103, 38.99),
+(004, 4, 104, 95.0), 
+(005, 5, 105, 87.5);
 
 
 INSERT INTO tests(id, type, percentage, student_id)
 VALUES
-(011, 'Mid-term', 97.0, 1), 
-(011, 'Final', 87.0, 1),     
-(022, 'Quiz', 78.5, 2),    
-(033, 'Mid-term', 88.0, 2),  
-(033, 'Final', 92.0, 3),
-(044, 'Final', 91.0, 4), 
-(005, 'Mid-term', 70.5, 5);
+(011, 'Mid-term', 97.0,1), 
+(011, 'Final', 87.0,1),     
+(022, 'Quiz', 78.5,2),    
+(033, 'Mid-term', 88.0,3),  
+(033, 'Final', 92.0,4),
+(044, 'Final', 91.0,4), 
+(005, 'Mid-term', 70.5,5);
 
 INSERT INTO test_results(id, course_result_id, test_result_id, grade)
 VALUES
