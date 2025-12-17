42d00523a27721df144c1df41ec2c0a386edaa68
22301016 <22301016@student.ciu.edu.tr>
2025-12-17 18:09:35 +0200

    fix the issues

--- liya/select.sql
+++ liya/select.sql
@@ -1,17 +1,4 @@
 SELECT * FROM transcripts;
-SELECT * FROM course_results;
-
-
-ALTER TABLE tests
-RENAME COLUMN percentage to score;
-SELECT * FROM tests;
-
-SELECT type, MAX(score) AS Max_score
-FROM tests
-GROUP BY type;
-
-
-
-ALTER TABLE test_results
-RENAME COLUMN grade to t_result;
 SELECT * FROM test_results;
+SELECT * FROM course_results;
+SELECT * FROM tests;
